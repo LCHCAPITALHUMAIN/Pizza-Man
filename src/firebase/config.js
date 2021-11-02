94a33941ba7dbc77caa6643024bd5191ef9d50c6
Daniel Valide <danielvalide@MacBook-Pro-de-Daniel.local>
2021-11-02 09:52:15 +0100

    First commit

---
+++ src/firebase/config.js
@@ -0,0 +1,33 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCbMJbiqbdCqYMxOjvAJqNC5ELhG-YA-qU",
+    authDomain: "guigal.firebaseapp.com",
+    projectId: "guigal",
+    storageBucket: "guigal.appspot.com",
+    messagingSenderId: "621801412216",
+    appId: "1:621801412216:web:f42ca5b1a220d955bb7f8d",
+    measurementId: "G-NC1W0XDRK5"
+  };
+  export default firebaseConfig;
+
+  /*
+  // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCbMJbiqbdCqYMxOjvAJqNC5ELhG-YA-qU",
+  authDomain: "guigal.firebaseapp.com",
+  projectId: "guigal",
+  storageBucket: "guigal.appspot.com",
+  messagingSenderId: "621801412216",
+  appId: "1:621801412216:web:f42ca5b1a220d955bb7f8d",
+  measurementId: "G-NC1W0XDRK5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);*/
